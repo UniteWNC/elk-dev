821f48ebf85df1d237e458de7b047f168f1561bb
Jeff Hallyburton <jeff.hallyburton@thomsonreuters.com>
2017-07-03 01:38:14 -0400

    Add x-pack plugin for kibana.

--- setup/scripts/bootstrap.sh
+++ setup/scripts/bootstrap.sh
@@ -20,9 +20,6 @@ yum install -y elasticsearch
 yum install -y kibana
 yum install -y logstash
 
-# Install dependencies for kibana plugins
-yum install -y git npm
-
 # Copy over application config
 cp -f /tmp/config/nginx/nginx.conf                /etc/nginx/nginx.conf
 cp -f /tmp/config/nginx/kibana.conf               /etc/nginx/conf.d/kibana.conf
@@ -30,29 +27,29 @@ cp -f /tmp/config/nginx/htpasswd.users            /etc/nginx/htpasswd.users
 cp -f /tmp/config/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 cp -f /tmp/config/kibana/kibana.yml               /etc/kibana/kibana.yml
 
+# Add symlinks for elasticsearch
+ln -s /usr/share/elasticsearch/bin/elasticsearch        /usr/local/sbin/
+ln -s /usr/share/elasticsearch/bin/elasticsearch-plugin /usr/local/sbin/
+
+# Add symlinks for kibana
+ln -s /usr/share/kibana/bin/kibana        /usr/local/sbin/
+ln -s /usr/share/kibana/bin/kibana-plugin /usr/local/sbin/
+
+# Configure plugins
+
+## X-Pack
+yes | elasticsearch-plugin install x-pack
+kibana-plugin install x-pack
+
 # Start services
 systemctl start nginx
 systemctl start elasticsearch
 systemctl start kibana
 systemctl start logstash
-
+ 
 # Enable services
 systemctl enable nginx
 systemctl enable elasticsearch
 systemctl enable kibana
 systemctl enable logstash
 
-# Add symlinks for kibana
-ln -s /usr/share/kibana/bin/kibana        /usr/local/sbin/
-ln -s /usr/share/kibana/bin/kibana-plugin /usr/local/sbin/
-
-# Configure sense and correct kibana permissions
-git clone https://github.com/dlumbrer/kbn_network.git /usr/share/kibana/plugins/network_vis
-cd /usr/share/kibana/plugins/network_vis/
-rm -rf images
-sed -i "s/5.4.0/5.4.3/g" package.json
-npm install
-
-# Restart services to enable plugins
-service kibana restart        && sleep 30
-service elasticsearch restart && sleep 30
